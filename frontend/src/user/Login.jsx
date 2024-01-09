5df8c0160107279e803045a1063377019d62c08b
minu <utopian4723@gmail.com>
2024-01-09 10:34:27 +0000

    v1.2.0

--- frontend/src/user/Login.jsx
+++ frontend/src/user/Login.jsx
@@ -1,68 +1,74 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const Login = () => {
-  const [id, setId] = useState('');
-  const [pw, setPw] = useState('');
-  const [session, setSession] = useState('');
+    const [id, setId] = useState("");
+    const [pw, setPw] = useState("");
+    const [session, setSession] = useState("");
 
-  const handleLogin = async () => {
-    try {
-      const response = await fetch('http://coin.oppspark.net:8088/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ id, pw }),
-      });
+    const handleLogin = async () => {
+        const response = await fetch("http://coin.oppspark.net:8088/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ id, pw }),
+        })
+            .then((res) => {
+                return res.json();
+            })
+            .then((data) => {
+                data.result === "login_success"
+                    ? setSession(data.session)
+                    : setSession("");
+            })
+            .catch((error) => {
+                console.error("로그인 요청 중 에러 발생:", error);
+            });
+    };
 
-      const data = await response.json();
+    const handleLogout = async () => {
+        try {
+            await fetch("http://coin.oppspark.net:8088/login", {
+                method: "DELETE",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
 
-      if (data.result === 'login_success') {
-        alert('로그인 성공!');
-      } else {
-        alert(`로그인 실패: ${data.result}`);
-      }
-    } catch (error) {
-      console.error('로그인 요청 중 에러 발생:', error);
-    }
-  };
+            alert("로그아웃 성공!");
+        } catch (error) {
+            console.error("로그아웃 요청 중 에러 발생:", error);
+        }
+    };
 
-  const handleLogout = async () => {
-    try {
-      await fetch('http://coin.oppspark.net:8088/login', {
-        method: 'DELETE',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-
-      alert('로그아웃 성공!');
-    } catch (error) {
-      console.error('로그아웃 요청 중 에러 발생:', error);
-    }
-  };
-
-
-  return (
-    <div>
-      <label>
-        ID:
-        <input type="text" value={id} onChange={(e) => setId(e.target.value)} />
-      </label>
-      <br />
-      <label>
-        PW:
-        <input type="password" value={pw} onChange={(e) => setPw(e.target.value)} />
-      </label>
-      <br />
-      <button type="button" onClick={handleLogin}>
-        로그인
-      </button>
-      <button type="button" onClick={handleLogout}>
-        로그아웃
-      </button>
-    </div>
-  );
+    return (
+        <div>
+            <label>
+                ID:
+                <input
+                    type="text"
+                    value={id}
+                    onChange={(e) => setId(e.target.value)}
+                />
+            </label>
+            <br />
+            <label>
+                PW:
+                <input
+                    type="password"
+                    value={pw}
+                    onChange={(e) => setPw(e.target.value)}
+                />
+            </label>
+            <br />
+            <button type="button" onClick={handleLogin}>
+                로그인
+            </button>
+            <button type="button" onClick={handleLogout}>
+                로그아웃
+            </button>
+        </div>
+    );
 };
 
 export default Login;
