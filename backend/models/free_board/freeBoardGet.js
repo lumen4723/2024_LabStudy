5e2b9b2ebabc50b429f2eb9bd1211ce142f4827e
OppSpark <picks5@naver.com>
2024-01-09 17:56:49 +0900

    freeBoardGet

--- backend/models/free_board/freeBoardGet.js
+++ backend/models/free_board/freeBoardGet.js
@@ -1,8 +1,28 @@
 const express = require('express');
 const router = express.Router();
 
-router.get("/free", (req, res) => {});
 
-router.get("/free/:id", (req, res) => {});
+router.get("/free", ( res) => {
+    const sql = "SEECT * FROM freeboard";
+    connection.query(sql,(err, rows) =>{
+        if(err) throw err;
+        
+        res.send(rows);
+    }); 
+});
+
+
+router.get("/free/:id", (req, res) => {
+
+    const sql = "SELECT * FROM freeboard WHERE id = ?";
+    const id = [req.body.id];
+
+    connection.query(sql, id, (err, rows) => {
+        if(err) throw err;
+        res.json(rows);
+    });
+});
+
+
 
 module.exports = router;
