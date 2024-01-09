6e67840af1afc9845b35232db31014b26e2b5b30
minu <utopian4723@gmail.com>
2024-01-09 07:49:09 +0000

    v1.1.5 login AND register 로직 구현

--- backend/models/authenticated/login.js
+++ backend/models/authenticated/login.js
@@ -5,29 +5,65 @@ const router = express.Router();
 
 //router.get("/login", (req, res) => {});
 
+const mysql = require("mysql");
+const dbconfig = require("../config/dbinfo.js");
+const connection = mysql.createConnection(dbconfig);
 
-app.post('/login', (req, res) => {
-    if (req.session.user ? req.session.user.id == 'test' : false) {
-        res.redirect('/');
-    }
-    else if(req.body.id == '' && req.body.pw == 'pw') {
-        req.session.user = {
-            id: req.body.id,
-        };
+app.post("/login", (req, res) => {
+    if (req.session.user) {
+        // already logged in
+        const sql = "SELECT * FROM user WHERE id = ? AND pw = ?";
+        const params = [req.session.user.id, req.session.user.pw];
 
-        res.setHeader('Set-Cookie', ['user=' + req.body.id]);
-        res.redirect('/');
-    }
-    else {
-        res.redirect('/login');
+        connection.query(sql, params, (err, rows) => {
+            if (err) throw err;
+
+            // session user 정보가 디비에 없으면 로그인 페이지로 이동
+            if (rows.length == 0) {
+                res.redirect("/login");
+            } else {
+                // session user 정보가 디비에 있으면 메인 페이지로 이동
+                req.session.user = {
+                    id: req.session.user.id,
+                    pw: req.session.user.pw,
+                };
+
+                res.setHeader("Set-Cookie", ["user=" + req.session.user.id]);
+                res.redirect("/");
+            }
+        });
+
+        res.redirect("/");
+    } else if (req.body.id && req.body.pw) {
+        const sql = "SELECT * FROM user WHERE id = ? AND pw = ?";
+        const params = [req.body.id, req.body.pw];
+
+        connection.query(sql, params, (err, rows) => {
+            if (err) throw err;
+
+            // request body에 user 정보가 디비에 없으면 로그인 페이지로 이동
+            if (rows.length == 0) {
+                res.redirect("/login");
+            } else {
+                // request body에 user 정보가 디비에 있으면 메인 페이지로 이동
+                req.session.user = {
+                    id: req.body.id,
+                    pw: req.body.pw,
+                };
+
+                res.setHeader("Set-Cookie", ["user=" + req.body.id]);
+                res.redirect("/");
+            }
+        });
+    } else {
+        res.redirect("/login");
     }
 });
 
-
-
-
-
-
-router.get("/logout", (req, res) => {});
+router.get("/logout", (req, res) => {
+    req.session.destroy();
+    res.clearCookie("user");
+    res.redirect("/");
+});
 
 module.exports = router;
--- backend/models/authenticated/register.js
+++ backend/models/authenticated/register.js
@@ -2,10 +2,17 @@ const express = require("express");
 const router = express.Router();
 
 router.post("/register", (req, res) => {
+    if (req.body.id && req.body.pw) {
+        const sql = "INSERT INTO user (id, pw) VALUES (?, ?)";
+        const params = [req.body.id, req.body.pw];
 
-    const user.save((err, UserInfo)) =>{
-
-
+        connection.query(sql, params, (err, rows) => {
+            if (err) throw err;
+            res.redirect("/login");
+        });
+    } else {
+        res.redirect("/register");
+    }
 });
 
 module.exports = router;
