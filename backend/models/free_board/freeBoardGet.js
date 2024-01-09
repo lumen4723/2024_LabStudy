6b55142a1ea14447d58314e487b243fefa8e78e8
OppSpark <picks5@naver.com>
2024-01-09 18:09:13 +0900

    freeBoardGet

--- backend/models/free_board/freeBoardGet.js
+++ backend/models/free_board/freeBoardGet.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const path = require("path");
+const mysql = require("mysql");
+const mainModulePath = path.dirname(require.main.filename);
+const dbconfig = require(path.resolve(mainModulePath, "../config/dbinfo.js"));
+const connection = mysql.createConnection(dbconfig);
 
 
 router.get("/free", ( res) => {
