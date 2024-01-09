a1ea2d1f17a704426772b14a1a8f0dc66dc62c30
minu <utopian4723@gmail.com>
2024-01-09 09:01:59 +0000

    v1.1.9

--- backend/models/free_board/freeBoardPost.js
+++ backend/models/free_board/freeBoardPost.js
@@ -1,7 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.post("/free", (req, res) => {});
+router.post("/free", (req, res) => {
+    const sql =
+        "INSERT INTO freeboard (title, content, userid)" + "VALUES (?, ?, ?)";
+    const params = [req.body.title, req.body.content, req.body.writer];
+
+    connection.query(sql, params, (err, rows) => {
+        if (err) throw err;
+        res.send({ result: "success" });
+    });
+});
 
 router.put("/free/:id", (req, res) => {});
 
