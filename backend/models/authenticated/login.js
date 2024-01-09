5438826ea14fe082fd434cb35c31f23bb8aa3b8f
OppSpark <picks5@naver.com>
2024-01-09 15:47:28 +0900

    Merge branch 'backend' of github.com:lumen4723/2024_LabStudy into backend

--- backend/models/authenticated/login.js
+++ backend/models/authenticated/login.js
@@ -3,9 +3,30 @@ const router = express.Router();
 
 //app.get("/", (req, res) => {});
 
-router.get("/login", (req, res) => {});
+//router.get("/login", (req, res) => {});
 
-router.post("/login", (req, res) => {});
+
+app.post('/login', (req, res) => {
+    if (req.session.user ? req.session.user.id == 'test' : false) {
+        res.redirect('/');
+    }
+    else if(req.body.id == '' && req.body.pw == 'pw') {
+        req.session.user = {
+            id: req.body.id,
+        };
+
+        res.setHeader('Set-Cookie', ['user=' + req.body.id]);
+        res.redirect('/');
+    }
+    else {
+        res.redirect('/login');
+    }
+});
+
+
+
+
+
 
 router.get("/logout", (req, res) => {});
 
--- backend/models/authenticated/register.js
+++ backend/models/authenticated/register.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-router.post("/register", (req, res) => {});
+router.post("/register", (req, res) => {
+
+    const user.save((err, UserInfo)) =>{
+
+
+});
 
 module.exports = router;
