0a847e7f89e107e03b86d629a7f2abdf52170225
jaehyuk <KT01097961319@gmail.com>
2024-01-10 21:27:12 +0900

    make header footer

--- frontend/src/lib/Header.jsx
+++ frontend/src/lib/Header.jsx
@@ -5,7 +5,7 @@ import { Link, useNavigate } from 'react-router-dom';
 const Header = () => {
     return (
 
-        <div>
+        <div classname = "HeaderDIV">
             <header>
                 <Link to="http://localhost:3000/">
                     <h1>코인라이프</h1>
