193b923add35b0dbde69b2b0102d9a3a2814f41d
minu <utopian4723@gmail.com>
2024-01-10 06:52:33 +0000

    minu-mod-02

--- frontend/src/user/Login.jsx
+++ frontend/src/user/Login.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const Login = () => {
     const [id, setId] = useState("");
@@ -7,46 +7,46 @@ const Login = () => {
     const navigate = useNavigate();
 
     const handleLogin = async () => {
-        const response = await fetch("http://coin.oppspark.net:8088/login", {
+        const response = await fetch("http://localhost:8088/login", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
             },
+            credentials: "include",
             body: JSON.stringify({ id, pw }),
-            })
+        })
             .then((res) => {
+                // 쿠키값 확인 코드
+                console.log(res.headers.get("Set-Cookie"));
                 return res.json();
             })
             .then((data) => {
-                if(data.result === "already_login") {
-                  navigate('/');
-                  console.log(data.result);
+                if (data.result === "already_login") {
+                    navigate("/");
+                    console.log(data.result);
+                } else if (data.result === "login_success") {
+                    navigate("/");
+                    console.log(data.result);
+                } else if (data.result === "no_user") {
+                    alert("id 혹은 pw가 맞지 않습니다.");
                 }
-                else if(data.result === "login_success") {
-                  navigate('/');
-                  console.log(data.result);
-                } 
-                else if(data.result === "no_user") {
-                  alert("id 혹은 pw가 맞지 않습니다.");
-                }
-                 
             })
             .catch((error) => {
                 console.error("로그인 요청 중 에러 발생:", error);
             });
     };
 
-    const handleLogout = async () => { await fetch("http://coin.oppspark.net:8088/login", {
+    const handleLogout = async () => {
+        await fetch("http://coin.oppspark.net:8088/login", {
             method: "DELETE",
             headers: {
                 "Content-Type": "application/json",
             },
-        })
-        .catch((error) => {
-          console.error("로그아웃 요청 중 에러 발생:", error);
-        })
+        }).catch((error) => {
+            console.error("로그아웃 요청 중 에러 발생:", error);
+        });
         alert("로그아웃 성공!");
-        navigate('/');
+        navigate("/");
     };
 
     return (
@@ -75,10 +75,8 @@ const Login = () => {
             <button type="button" onClick={handleLogout}>
                 로그아웃
             </button>
-            <Link to = "/signup">
-              <button type="button">
-                회원가입
-              </button>
+            <Link to="/signup">
+                <button type="button">회원가입</button>
             </Link>
         </div>
     );
--- frontend/src/user/Signup.jsx
+++ frontend/src/user/Signup.jsx
@@ -1,60 +1,70 @@
 import "./Signup.css";
 
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const Signup = () => {
-  const [formData, setFormData] = useState({
-    id: '',
-    pw: '',
-  });
-
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
+    const [formData, setFormData] = useState({
+        id: "",
+        username: "",
+        pw: "",
     });
-  };
 
-  const handleSignup = async () => {
-    
-    const response = await fetch('http://coin.oppspark.net:8088/register', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData),
-      })
-      .catch((error) => {
-        console.log('회원가입 오류');
-      }).then((data) => {
-        if(data.result === 'invaild_value') {
-            alert("id 또는 password 값을 입력하세요. ");
-            console.log(data.result);
-        }
-        if(data.result === 'register_success') {
-            alert("회원가입 완료되었습니다. 로그인하세요.")
-            navigator('/login');
-        }
-      });
-    
-  };
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
 
-  return (
-    <div className="signup">
-      <label>
-        ID:
-        <input type="text" name="id" value={formData.id} onChange={handleInputChange} />
-      </label>
-      <br />
-      <label>
-        Password:
-        <input type="password" name="pw" value={formData.pw} onChange={handleInputChange} />
-      </label>
-      <br />
-      <button onClick={handleSignup}>회원가입</button>
-    </div>
-  );
+    const handleSignup = async () => {
+        const response = await fetch("http://coin.oppspark.net:8088/register", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(formData),
+        })
+            .catch((error) => {
+                console.log("회원가입 오류");
+            })
+            .then((data) => {
+                if (data.result === "invaild_value") {
+                    alert("id 또는 password 값을 입력하세요. ");
+                    console.log(data.result);
+                }
+                if (data.result === "register_success") {
+                    alert("회원가입 완료되었습니다. 로그인하세요.");
+                    navigator("/login");
+                }
+            });
+    };
+
+    return (
+        <div className="signup">
+            <label>
+                ID:
+                <input
+                    type="text"
+                    name="id"
+                    value={formData.id}
+                    onChange={handleInputChange}
+                />
+            </label>
+            <br />
+            <label>
+                Password:
+                <input
+                    type="password"
+                    name="pw"
+                    value={formData.pw}
+                    onChange={handleInputChange}
+                />
+            </label>
+            <br />
+            <button onClick={handleSignup}>회원가입</button>
+        </div>
+    );
 };
 
 export default Signup;
