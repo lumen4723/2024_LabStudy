bfe39b11139c3f74207bb16391cb6568d3b96e14
kang-dani <105360184+kang-dani@users.noreply.github.com>
2024-01-10 13:39:31 +0900

    dani-mod-05

--- frontend/src/free/FreeboardWrite.jsx
+++ frontend/src/free/FreeboardWrite.jsx
@@ -12,7 +12,9 @@ const FreeboardWrite = () => {
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({ title, content }) 
+            body: JSON.stringify({ title, content })
+        }).then((res) => {
+            return res.json();
         }).then((data) => {
             switch (data.result) {
                 case "no_session":
