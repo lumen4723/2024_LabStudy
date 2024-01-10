e53aec55080514ee83c272f9c2d64239f36a87bb
jaehyuk <KT01097961319@gmail.com>
2024-01-10 20:32:29 +0900

    Header.jsx Header.css Footer.jsx Footer.css

--- frontend/src/lib/Header.jsx
+++ frontend/src/lib/Header.jsx
@@ -1,10 +1,36 @@
-import "./Header.css";
+import './Header.css';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <header>
-            <h1>Header</h1>
-        </header>
+
+        <div>
+            <header>
+                <Link to="http://localhost:3000/">
+                    <h1>코인라이프</h1>
+                </Link>
+                <ul>
+                    <li>
+                        <Link to="/coinchart">코인차트</Link>
+                    </li>
+                    <li>
+                        <Link to="/freeboard">자유게시판</Link>
+                    </li>
+                    <li>
+                        <Link to="/qnaboard">질문게시판</Link>
+                    </li>
+                </ul>
+
+                <div className="list"></div>
+                <div className="sign">
+                    <Link to="/login">
+                        <p>로그인</p>
+                    </Link>
+                </div>
+            </header>
+            <hr />
+        </div>
     );
 };
 
