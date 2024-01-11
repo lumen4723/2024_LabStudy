1da43e83055c913232dd25c2f90d9d7f3e6b7987
minu <utopian4723@gmail.com>
2024-01-11 06:57:58 +0000

    v.1.5.1 coinchart remake

--- frontend/src/free/Freeboard.jsx
+++ frontend/src/free/Freeboard.jsx
@@ -33,13 +33,22 @@ const Freeboard = () => {
             <h1 className="freetitle">게시글 목록</h1>
             <ul className="post-list">
                 {posts.map((post) => (
-                    <li className="post" key={post.id}>
+                    <li className="postfree" key={post.id}>
                         <Link
                             className="detailbtn"
                             to={`/freeboard/${post.id}`}
                         >
                             {post.title}
                         </Link>
+                        <Link
+                            className="detailbtn"
+                            to={`/freeboard/${post.id}`}
+                        >
+                            {post.content}
+                        </Link>
+                        <span className="author">{post.userid}</span>
+                        <span className="created">{post.created}</span>
+                        <span className="view">{post.view}</span>
                     </li>
                 ))}
             </ul>
