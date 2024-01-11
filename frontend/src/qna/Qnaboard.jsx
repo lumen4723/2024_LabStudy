d55db62e3b29efe54fa1cf89d228b9991e2cb52b
kang-dani <105360184+kang-dani@users.noreply.github.com>
2024-01-11 16:12:22 +0900

    dani-QnAB-mod01

--- frontend/src/qna/Qnaboard.jsx
+++ frontend/src/qna/Qnaboard.jsx
@@ -33,14 +33,23 @@ const Qnaboard = () => {
             <h1 className="qnatitle">질문 게시글 목록</h1>
             <ul className="post-list">
                 {posts.map((post) => (
-                    <li className="post" key={post.id}>
-                        <Link
-                            className="detailbtn"
-                            to={`/qnaboard/${post.id}`}
-                        >
-                            {post.title}
-                        </Link>
-                    </li>
+                    <li className="postqna" key={post.id}>
+                    <Link
+                        className="detailbtn"
+                        to={`/qnaboard/${post.id}`}
+                    >
+                        {post.title}
+                    </Link>
+                    <Link
+                        className="detailbtn"
+                        to={`/qnaboard/${post.id}`}
+                    >
+                        {post.content}
+                    </Link>
+                    <span className="author">{post.userid}</span>
+                    <span className="created">{post.created}</span>
+                    <span className="view">{post.view}</span>
+                </li>
                 ))}
             </ul>
             <Link className="writebtn" to="/qnaboard/write">
