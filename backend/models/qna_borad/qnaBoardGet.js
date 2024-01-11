82bbb20994ff8108d0e60c43eb97207e355dce5b
kang-dani <105360184+kang-dani@users.noreply.github.com>
2024-01-11 14:50:21 +0900

    dani-qnaBG-mod00

--- backend/models/qna_borad/qnaBoardGet.js
+++ backend/models/qna_borad/qnaBoardGet.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
-router.get("/qna", (req, res) => {});
+const path = require("path");
+const mysql = require("mysql");
+const mainModulePath = path.dirname(require.main.filename);
+const dbconfig = require(path.resolve(mainModulePath, "../config/dbinfo.js"));
+const connection = mysql.createConnection(dbconfig);
 
-router.get("/qna/:id", (req, res) => {});
+router.get("/qna", (req, res) => {
+    const sql = "SELECT * FROM qnaboard";
+
+    connection.query(sql, (err, rows) => {
+        return res.send(rows);
+    });
+});
+
+router.get("/qna/:id", (req, res) => {
+    const sql = "SELECT * FROM qnaboard WHERE id = ?";
+    const id = [req.params.id];
+
+    connection.query(sql, id, (err, rows) => {
+        return res.send(rows);
+    });
+});
 
 module.exports = router;
